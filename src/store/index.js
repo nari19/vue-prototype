1df46de8682e4855218d645505a24a21ba642cfa
nari19 <kazuki7793996@gmail.com>
2018-12-11 13:12:41 +0900

    new modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import basicModal from './modules/basicModal'
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
-});
+// https://qiita.com/RikutoYamaguchi/items/2fb1c1dc8be196dfc883
+const state = {
+}
 
+const modules = {
+  basicModal
+}
+
+const store = new Vuex.Store({
+  state,
+  modules
+})
+
+export default store
